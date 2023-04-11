3df361971952c0d6176652af00cb64b39f7c5f67
P. Michael Holland <maikeru.horando@gmail.com>
2023-04-11 16:15:49 +0200

    Implement initial GuessInput

---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+function GuessInput() {
+  const [guess, setGuess] = React.useState("");
+
+  const handleInput = (e) => {
+    const newGuess = (e.target.value ?? "").toUpperCase();
+    setGuess(newGuess);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log({ guess });
+    setGuess("");
+  };
+
+  return (
+    <form className="guess-input-wrapper" onSubmit={handleSubmit}>
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        id="guess-input"
+        type="text"
+        onChange={handleInput}
+        value={guess}
+        pattern="\w{5,5}"
+        title="5 letter word"
+      />
+    </form>
+  );
+}
+
+export default GuessInput;
